87a107bd58692f521780676f63b7303e68bab8ca
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-03-09 00:48:16 +0900

    最低限のSwaggerの仕組みを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+
+function buildDocument(app: INestApplication) {
+  const config = new DocumentBuilder().setTitle('Tweeter API Document').setVersion('1.0').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  buildDocument(app)
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
