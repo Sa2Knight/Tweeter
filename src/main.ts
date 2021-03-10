fb81b13c5c32c39679934d305e4fae0e1ce7f614
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-03-10 21:17:01 +0900

    Revert "最低限のSwaggerの仕組みを追加"
    This reverts commit 87a107bd58692f521780676f63b7303e68bab8ca.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
-import { INestApplication } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { AppModule } from './app.module'
-
-function buildDocument(app: INestApplication) {
-  const config = new DocumentBuilder().setTitle('Tweeter API Document').setVersion('1.0').build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('swagger', app, document)
-}
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  buildDocument(app)
-  await app.listen(3000)
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-bootstrap()
+bootstrap();
