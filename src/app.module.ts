324223677061f9ee728766081dbb60927c38707d
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-03-14 13:01:17 +0900

    Entities を動的に読み込むように修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,14 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { Tweet } from './entities/tweet.entity'
-import { User } from './entities/user.entity'
 import { UsersModule } from './users/users.module'
 import { TweetController } from './tweet/tweet.controller'
 import { TweetService } from './tweet/tweet.service'
 import { AuthModule } from './auth/auth.module'
-import { Followings } from './entities/followings.entity'
 import { FollowingsController } from './followings/followings.controller'
 import { FollowingsService } from './followings/followings.service'
 import { BookmarkController } from './bookmark/bookmark.controller'
 import { BookmarkService } from './bookmark/bookmark.service'
-import { Bookmark } from './entities/bookmark.entity'
 
 @Module({
   imports: [
@@ -23,7 +19,7 @@ import { Bookmark } from './entities/bookmark.entity'
       port: 3306,
       username: 'root',
       database: `tweeter_${process.env.NODE_ENV}`,
-      entities: [User, Tweet, Followings, Bookmark],
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: true
     }),
     UsersModule,
